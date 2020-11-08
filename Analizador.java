51bb814bacafad1941a7757f7f6c3f1dc13afd12
edu2000 <edu3000@ciencias.unam.mx>
2020-11-07 18:50:55 -0600

    Mi primer commit "Formal"

---
+++ Analizador.java
@@ -0,0 +1,210 @@
+/**
+* @author Eduardo Montaño Gomez
+* Numero de cuenta: 421005183
+* @version 1.0 Noviembre 2020
+* @since ICC laboratorio 2021-1 
+*/
+
+public class Analizador implements AnalizadorDeCadenas{
+
+	public Analizador(){}//Es un constructor vacio para inicializar los obejtos
+	
+	//ACTIVIDAD 1
+	@Override	
+	public int encuentraPosicion(String cadena, char c) {
+		int posicion = 0;
+		for(int i = 0; i<cadena.length(); i++) {
+			char c1 = cadena.charAt(i);//Declaramos una variable local de tipo char en cada iteracion, donde la variable guarda el caracter i-esimo caracter de la cadena
+			if (c1 == c) {
+				posicion = i;
+				break; //Uso break para que no siga iterando
+			}else { posicion = -1; }
+		}
+		return posicion;
+	}
+
+	//ACTIVIDAD 2
+	@Override
+	public boolean esNumero(String cadena) {
+		boolean numero = false;
+		int numPuntos = 0;
+
+		for(int i = 0; i<cadena.length(); i++) {
+			if(cadena.charAt(i) == '.')
+				numPuntos+= 1;//Va acumulando la cantidad de puntos del String
+		}
+
+		if(numPuntos <= 1) {//Valida si es que se han introducido mas de dos puntos
+			for(int i = 0; i<cadena.length(); i+=1) {
+
+				switch (cadena.charAt(i)) {
+					case('0'): numero = true;
+						break;
+					case('1'): numero = true;
+						break;
+					case('2'): numero = true;
+						break;
+					case('3'): numero = true;
+						break;
+					case('4'): numero = true;
+						break;
+					case('5'): numero = true;
+						break;
+					case('6'): numero = true;
+						break;
+					case('7'): numero = true;
+						break;
+					case('8'): numero = true;
+						break;
+					case('9'): numero = true;
+						break;
+					case('.'): numero = true;
+						break;
+					default: numero = false;
+						break;
+				}//Fin del switch
+
+				if (numero == false)
+					break;
+			}
+		}else {numero = false;}
+
+		return numero;
+	}
+
+	//ACTIVIDAD 3
+	@Override
+	public String reemplazaCaracter(String cadena, char reemplazado, char reemplazador) {
+
+		int i = 0;
+		String cadenaModificada = "";//Declaramos una variable para almacenar la cadena modificada
+		while (i<cadena.length()) {
+			if (cadena.charAt(i) == reemplazado) {//Comparamos el caracter i con el reemplazado
+				cadenaModificada += "" + reemplazador; //Si es cierto, a la cadena modificada le concatenamos el caracter reemplazador
+			}else {
+				cadenaModificada += "" + cadena.charAt(i);//Si no es igual, le concatenamos el caracter i-esimo de la cadena original
+			}
+			i++;//Sumamos 1 a i
+		}
+		return cadenaModificada;
+	}
+
+	//ACTIVIDAD 4
+	@Override
+	public boolean esPalindromo(String cadena) {
+		String cadenaMinusculas = cadena.toLowerCase();
+		String cadena2 = "";
+		String cadena3 = "";
+		boolean sonIguales = false;
+
+		for(int i = 0; i<cadenaMinusculas.length(); i++) {
+			if (cadenaMinusculas.charAt(i) != ' ') {
+				cadena2 += ""+cadenaMinusculas.charAt(i);
+			}
+		}
+		
+		int i = 0;
+		while(i<cadena2.length()){
+			cadena3 += "" + cadena2.charAt(cadena2.length()-i-1);
+			i++;
+		}
+		//Para este momento, cadena3 ya tiene lo que tenia cadena 2 pero al reves
+		//Verificamos si la longitud de la cadena 2 es 0, si es cero, entonces es palindromo.
+		if (cadena2.length() == 0) {
+				sonIguales = true;
+		}else {
+			for(int j = 0; j<cadena2.length(); j++) {
+			
+				if(cadena2.charAt(j) == cadena3.charAt(j)) {
+					sonIguales = true;
+				}else { 
+					sonIguales = false;
+					break;
+				}
+			}
+		}
+		return sonIguales;
+	}
+
+	//ACTIVIDAD 5
+	@Override
+	public boolean contiene(String cadena, String buscada) {
+		//System.out.println(buscada.charAt(0));
+		boolean siContiene = false;//Variable booleana para saber si lo contiene o no
+		if (cadena.length() < buscada.length()) {//Preguntamos si es que la cadena buscada es mayor que la cadena contenedora
+			siContiene = false; //si es mayor, entonces no puede estar contenida en otra mas pequena y por lotanto es falso
+			System.out.println("La cadena buscada es mayor!!: ");
+		}else { //Si la cadena a buscar no es mas grande, entonces
+			for (int i = 0; i<cadena.length(); i+=1) {//Hacemos un ciclo con la longitud de caracteres de la cadena contenedora
+				if(cadena.charAt(i) == buscada.charAt(0)) {//Verificamos si el caracter i-esimo de la cadena contenedora es igual al primer caracter de la cadena a buscar
+					if (cadena.substring(i).length() < buscada.length()) {
+							siContiene = false;
+							break;
+					}else {
+						String cadena2 = cadena.substring(i, i + buscada.length());//Si coincide, declaramos otra vairable String local y le asignamos los caracteres de la cadena mayor, desde el caracter que coincidio hasta los que completen la longitud de la cadena buscada
+
+						for(int j = 0; j<buscada.length(); j++){//Hacemos otro for, en este verificamos si todos los caracteres coiniciden
+							if(cadena2.charAt(j) == buscada.charAt(j)){//Si el i-esimo caracter coincide, entonces la variable siContiene se evalua a verdadero.
+								siContiene = true;
+							}else {//Si no lo contiene, entonces es falso siContiene
+								siContiene = false;//Si un caracter no coincide, la variable siContiene es falsa.
+								break;//Con que un caracter sea falso basta para que las dos cadenas no sean iguales, y hace un break para salir del for
+							}
+						}
+					}
+				}
+			}
+		}
+		return siContiene;
+	}
+
+	public static void main(String[] args) {
+		Analizador analizador = new Analizador();
+
+
+		////////// ACTIVIDAD 1//////////////////
+		System.out.println("Posicion: " + analizador.encuentraPosicion("Esta es una palabra de prueba”,",'e'));
+		System.out.println("Posicion: " + analizador.encuentraPosicion("Esta es una palabra de prueba”,",'E'));
+		System.out.println("Posicion: " + analizador.encuentraPosicion("Esta es una palabra de prueba”,",'z'));
+		System.out.println("Posicion: " + analizador.encuentraPosicion("Esta es una palabra de prueba”,",'u'));
+		////////////////////////////////////////
+		////////// ACTIVIDAD 2//////////////////
+		System.out.println("------------------------------------------");
+		System.out.println(analizador.esNumero("Texto"));
+		System.out.println(analizador.esNumero("133"));
+		System.out.println(analizador.esNumero("12.44"));
+		System.out.println(analizador.esNumero("12.44.3.2"));
+		System.out.println(analizador.esNumero("1233texto42"));
+		System.out.println("------------------------------------------");
+		////////////////////////////////////////
+		///////// ACTIVIDAD 3//////////////////
+		System.out.println(analizador.reemplazaCaracter("Hola Mundo",'o', 'i'));
+		System.out.println(analizador.reemplazaCaracter("Palabra de prueba",'a', 'o'));
+		System.out.println(analizador.reemplazaCaracter("Palabra de prueba",'P', 'H'));
+		System.out.println(analizador.reemplazaCaracter("Palabra de prueba",'p', 'w'));
+		System.out.println(analizador.reemplazaCaracter("Palabra de prueba",' ', '_'));
+		System.out.println(analizador.reemplazaCaracter("Palabra de prueba",' ', '\0'));
+		System.out.println("------------------------------------------");
+		///////////////////////////////////////
+		///////// ACTIVIDAD 4//////////////////
+		//System.out.println(analizador.esPalindromo("Anital avalatIna"));
+		System.out.println(analizador.esPalindromo("oso baboso"));
+		System.out.println(analizador.esPalindromo("o"));
+		System.out.println(analizador.esPalindromo("Anita lava la tina"));
+		System.out.println(analizador.esPalindromo("Oso baboso"));
+		System.out.println(analizador.esPalindromo("Hola mundo"));
+		//System.out.println(analizador.esPalindromo("dsfsdfdsreth"));
+		System.out.println(analizador.esPalindromo(" "));
+		//System.out.println(analizador.esPalindromo("Palabra de prueba"));
+		System.out.println("------------------------------------------");
+		///////////////////////////////////////
+		///////// ACTIVIDAD 5//////////////////
+		System.out.println(analizador.contiene("Hola mundo", "mundo"));
+		System.out.println(analizador.contiene("Hola mundo", "oso"));
+		System.out.println(analizador.contiene("Hola", "Hola mundo"));
+		System.out.println(analizador.contiene("Hola mundo", "und"));
+		System.out.println("------------------------------------------");
+		///////////////////////////////////////
+	}
+
+}//Find de la clase
---
+++ README.md
@@ -0,0 +1,28 @@
+Nombre: Eduardo Montaño Gomez
+Nombre Proyecto: Practica 3 ICC
+Lenguaje(s) de programacion: Java
+
+Clases utilizadas:
+Analizador.java
+
+Intefraces Utilizadas:
+AnalizadorDeCadenas.java
+
+Descripcion del programa: El proyecto consiste en seis pequenias 
+actividades en cada una se especifica un tarea en particular.
+
+Actividad 1: Crear un metodo no estatico  que permita encontrar la posicion de un 
+caracter dado a partir de un objeto.
+
+Actividad 2: Metodo no estatico que permita determinar si una cadena de caracteres 
+es un numero.
+
+Actividad 3: Metodo no estatico que permita reemplazar un caracter por 
+otro dentro de una cadena de texto.
+
+Actividad 4: Metodo no estatico que permite decidir si una cadena de 
+texto es una palabra palindroma.
+
+Actividad 5: Metodo (no estatico) que permite saber si una cadena de 
+texto esta contenida dentro de otra.
+
